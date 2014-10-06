71d293163a2a9e498491af248d93f2088c89f564
mayuri122222 <mayurisupe@gmail.com>
2014-10-06 18:38:35 +0530

    confirmation mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -34,4 +34,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  # Gmail SMTP server setup
+  config.action_mailer.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :enable_starttls_auto => true,
+        :port => 587,
+        :domain => 'testmailkipl0@gmail.com',
+        :authentication => :plain,
+        :user_name => 'testmailkipl0',
+        :password => 'kipl12345'
+  }
 end
