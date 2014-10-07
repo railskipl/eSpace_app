31f6076954b8d69ddf38b35973b05115016af3dc
mayuri122222 <mayurisupe@gmail.com>
2014-10-07 11:19:39 +0530

    gmail confirmation

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,9 @@
  ActionMailer::Base.smtp_settings = {
+ 	:domain               => "gmail.com",
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => "testmailkipl0@gmail.com",
-    :password             => 'kipl12345',
+    :password             => 'kipl123456789',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/dashboard'
+
   devise_for :users
   root :to => 'home#index'
 
