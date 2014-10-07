0eb5eefe161170773a0306ea866cb9f23d51f731
Amol <rails@kunalinfotech.net>
2014-10-07 10:49:10 +0530

    Action mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,10 @@ Rails.application.configure do
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { :host => 'http://art-insurance.herokuapp.com' }
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
