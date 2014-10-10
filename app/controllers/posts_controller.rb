e966d5c34c669607d12bd4f27491d51021f3b4ea
Amol <rails@kunalinfotech.net>
2014-10-10 12:50:54 +0530

    Before filter authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  before_filter :authenticate_user!, :except => []
   before_action :set_product, only: [:show, :edit, :update, :destroy, :toggle]
 
   # GET /products
