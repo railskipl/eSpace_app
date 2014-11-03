e4d5b860ee428151a7d7123adb5725b5dc7a3b8b
Amol <rails@kunalinfotech.net>
2014-11-03 16:32:48 +0530

    Back details model updated

--- db/migrate/20141009085853_create_bank_details.rb
+++ db/migrate/20141009085853_create_bank_details.rb
@@ -2,9 +2,8 @@ class CreateBankDetails < ActiveRecord::Migration
   def change
     create_table :bank_details do |t|
       t.string :full_name
-      t.string :stripe_card_id_token
-      t.string :stripe_recipient_token
       t.string :card_number
+
       t.timestamps
     end
   end
