863f6fbaa2e549d0803a00fe793a71c6fb0d4102
railskipl <rails@kunalinfotech.net>
2014-11-04 16:40:25 +0530

    facebook congiguration change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,20 +1,20 @@
 
 
-#For Local machine keys
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  
-  provider :facebook, '382895341863463', '70e00b19e5cf11f56990e9402da7e8f5'
-  
-end
-
-# # For heroku server keys
+# #For Local machine keys
 # OmniAuth.config.logger = Rails.logger
-# OmniAuth.config.full_host = "http://store-finding-system.herokuapp.com"
+
 # Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook, "463439180461791", "da2a8a7e85b8c3eda15f129204dd9d23"
+  
+#   provider :facebook, '382895341863463', '70e00b19e5cf11f56990e9402da7e8f5'
+  
 # end
+
+# For heroku server keys
+OmniAuth.config.logger = Rails.logger
+OmniAuth.config.full_host = "http://store-finding-system.herokuapp.com"
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, "463439180461791", "da2a8a7e85b8c3eda15f129204dd9d23"
+end
 
 
 
