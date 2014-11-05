e29d942e6ca690257197acb53570e7b6bc407f7f
Amol <rails@kunalinfotech.net>
2014-11-05 12:37:26 +0530

    Conflict removed

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20141031115610) do
 
-  create_table "authentication_providers", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "authentication_providers", ["name"], name: "index_name_on_authentication_providers", using: :btree
-
-ActiveRecord::Schema.define(version: 20141103073929) do
-
   create_table "bank_details", force: true do |t|
     t.string   "full_name"
+    t.string   "stripe_card_id_token"
+    t.string   "stripe_recipient_token"
     t.string   "card_number"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -47,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141103073929) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stars",      default: 0
     t.integer  "rating"
     t.integer  "user_id"
   end
@@ -112,7 +105,7 @@ ActiveRecord::Schema.define(version: 20141103073929) do
   end
 
   create_table "ratings", force: true do |t|
-    t.decimal  "value"
+    t.decimal  "value",      precision: 10, scale: 0
     t.integer  "vote_count"
     t.integer  "item_id"
     t.string   "item_type"
@@ -122,8 +115,8 @@ ActiveRecord::Schema.define(version: 20141103073929) do
     t.datetime "updated_at"
   end
 
-  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type"
-  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type"
+  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type", using: :btree
+  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type", using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "name"
@@ -134,21 +127,7 @@ ActiveRecord::Schema.define(version: 20141103073929) do
     t.datetime "updated_at"
   end
 
-  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id"
-
-  create_table "user_authentications", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "authentication_provider_id"
-    t.string   "uid"
-    t.string   "token"
-    t.datetime "token_expires_at"
-    t.text     "params"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
-  add_index "user_authentications", ["authentication_provider_id"], name: "index_user_authentications_on_authentication_provider_id", using: :btree
-  add_index "user_authentications", ["user_id"], name: "index_user_authentications_on_user_id", using: :btree
+  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -167,9 +146,9 @@ ActiveRecord::Schema.define(version: 20141103073929) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.boolean  "admin",                  default: false
-    t.boolean  "status"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "status"
     t.string   "name"
     t.string   "last_name"
     t.string   "personal_email"
@@ -177,12 +156,10 @@ ActiveRecord::Schema.define(version: 20141103073929) do
     t.string   "mobile_number"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.integer  "admin_user_id"
-
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
