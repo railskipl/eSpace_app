68ad44b8754963f567cdf194b981bdd1cf6a1ac0
Amol <rails@kunalinfotech.net>
2014-11-26 16:12:03 +0530

    Searching on home page

--- app/models/about_u.rb
+++ app/models/about_u.rb
@@ -1,5 +1,14 @@
 class AboutU < ActiveRecord::Base
-	has_attached_file :photo, :styles => { :thumb => "91x61", :medium => "512x344" }
+
+  has_attached_file :photo, :styles => { :thumb => "61x61" },
+
+  			:storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml",
+                    :path => "/estore_management/posts/:id/:style/:basename.:extension",
+                    
+                    :convert_options => {
+                          :thumb => "-compose Copy -gravity center -extent 61x61"   
+                      }
+
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end
