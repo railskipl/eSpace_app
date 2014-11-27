7fc81036737be7185bf33a1ecc852c2ade6b40b3
Amol <rails@kunalinfotech.net>
2014-11-27 19:08:43 +0530

    Access control - Admin

--- app/controllers/access_ids_controller.rb
+++ app/controllers/access_ids_controller.rb
@@ -1,5 +1,6 @@
 class AccessIdsController < ApplicationController
-  before_filter :authenticate_user!, :except => []
+  before_filter :authenticate_user!, except: []
+  before_filter :correct_user, :except => []
   
   before_action :set_access, only: [:show, :edit, :update, :destroy]
   respond_to :html, :xml, :json
@@ -42,5 +43,10 @@ class AccessIdsController < ApplicationController
 	    def access_id_params
 	      params.require(:access_id).permit(:email)
 	    end
+
+	    def correct_user
+      		@user = User.find_by_id_and_admin(current_user.id, true)
+      		redirect_to(root_path, :notice => "Sorry, you are not allowed to access that page.") unless current_user=(@user)
+    	end
 
 end
