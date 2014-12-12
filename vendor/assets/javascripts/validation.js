93666313de1941ec2d70815066d0ee2598ac13e8
Amol <rails@kunalinfotech.net>
2014-12-12 12:40:09 +0530

    Design changes

--- vendor/assets/javascripts/validation.js
+++ vendor/assets/javascripts/validation.js
@@ -380,16 +380,6 @@ jQuery("#booking").validate({
 
 
 
-	jQuery('input').live('blur', function() {
-		
-        if (jQuery("#booking").valid()) {
-        	
-            jQuery('#customButton').removeAttr('disabled'); 
-        } else {
-            jQuery('#customButton').attr('disabled', 'disabled');
-        }
-    });
-
 
 
  jQuery("#reply_message").validate({
