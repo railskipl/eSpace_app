2050e818fbad4ab037f518be32c9418c69db2c91
kipl <kipl@kipl.com>
2014-12-16 15:28:02 +0530

    pull changes

--- db/schema.rb
+++ db/schema.rb
@@ -156,7 +156,7 @@ ActiveRecord::Schema.define(version: 20141215064457) do
   end
 
   create_table "ratings", force: true do |t|
-    t.decimal  "value",      precision: 10, scale: 0
+    t.decimal  "value"
     t.integer  "vote_count"
     t.integer  "item_id"
     t.string   "item_type"
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 20141215064457) do
     t.datetime "updated_at"
   end
 
-  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type", using: :btree
-  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type", using: :btree
+  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type"
+  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type"
 
   create_table "reviews", force: true do |t|
     t.string   "name"
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 20141215064457) do
     t.datetime "updated_at"
   end
 
-  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id", using: :btree
+  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -209,8 +209,8 @@ ActiveRecord::Schema.define(version: 20141215064457) do
     t.datetime "oauth_expires_at"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
