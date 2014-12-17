b54cf7111e63bbc030f3176d5fec21148a48f8e9
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-12-17 15:35:39 +0530

    changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,21 +5,20 @@ class Users::SessionsController < Devise::SessionsController
  end
 
  def create
-    self.resource = warden.authenticate!(auth_options)
-    if resource.status == false
+  self.resource = warden.authenticate!(auth_options)
+  if resource.status == false
     sign_out current_user
     redirect_to root_url,:notice => "Your account is blocked,please contact admin"
-    else
+  else
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-
     if !session[:return_to].blank?
-    redirect_to session[:return_to]
-    session[:return_to] = nil
+      redirect_to session[:return_to]
+      session[:return_to] = nil
     else
-    respond_with resource, :location => after_sign_in_path_for(resource)
+      respond_with resource, :location => after_sign_in_path_for(resource)
     end
-    end
+  end
 end
   # GET /resource/sign_in
   # def new
