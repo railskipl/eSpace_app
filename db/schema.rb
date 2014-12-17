6cdd7d090644e12144b70cc8da7302c94a549328
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-12-17 10:42:01 +0530

    chnages

--- db/schema.rb
+++ db/schema.rb
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 20141216123750) do
     t.datetime "updated_at"
   end
 
-  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type"
-  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type"
+  add_index "ratings", ["item_id", "item_type"], name: "index_ratings_on_item_id_and_item_type", using: :btree
+  add_index "ratings", ["rater_id", "rater_type"], name: "index_ratings_on_rater_id_and_rater_type", using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "name"
@@ -178,7 +178,7 @@ ActiveRecord::Schema.define(version: 20141216123750) do
     t.datetime "updated_at"
   end
 
-  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id"
+  add_index "reviews", ["post_id"], name: "index_reviews_on_post_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                           default: "",    null: false
@@ -211,8 +211,8 @@ ActiveRecord::Schema.define(version: 20141216123750) do
     t.boolean  "notification_for_personal_email"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
