8e033453db90345bcb8ed4ca56cecf6eab2b6d7f
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-12-18 13:21:21 +0530

    bug fixed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :poster,:class_name => 'User'
 	belongs_to :user
+
+	def self.result_area(post)
+		select("area").where("post_id = ? and pickup_date >= ?", post.id, Date.today)
+	end
 end
