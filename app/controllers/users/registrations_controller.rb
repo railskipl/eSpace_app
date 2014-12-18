3148dedef7d2570ba6c42ccade0dd8ff774bc63c
Amol <rails@kunalinfotech.net>
2014-12-18 14:38:52 +0530

    Password confirmation page updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,14 +9,35 @@ respond_to :html, :js
     super
 
     respond_to do |format|
-          format.js
+      format.js
     end
   end
 
   # POST /resource
   def create
-    super
+    build_resource(sign_up_params)
+    @resource_saved = resource.save
+
+    yield resource if block_given?
+
+    if @resource_saved
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      
+      respond_with resource
+    end
   end
+
+  
 
   # GET /resource/edit
   def edit
