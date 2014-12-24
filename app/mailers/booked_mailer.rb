784e74515d0c04034f886093a843daad01bb4b17
Amol <rails@kunalinfotech.net>
2014-12-24 12:40:01 +0530

    Finder receive pdf receipt after booked a space

---
+++ app/mailers/booked_mailer.rb
@@ -0,0 +1,20 @@
+class BookedMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def booked_a_spaces(booking)
+  	@booking = booking
+  	
+  	# email1 = @booking.user.email
+   #  email2 = @booking.poster.email
+
+    subject = "Booked space - Dinoba"
+
+  	attachments["Booked_space.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "receipt", :template => 'booked_mailer/booked_a_spaces.pdf.erb'))
+
+  	mail(:subject => subject, :to => "amol@kunalinfotech.net", :bcc => "amol@kunalinfotech.net") do |format|
+      format.html
+    end
+
+  end
+
+end
