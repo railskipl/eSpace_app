b38a17ea9dfd006f413a993fbb22480d92e33c12
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-12-24 19:08:04 +0530

    updated changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,11 +66,11 @@ class PostsController < ApplicationController
 
   def overview
       if params[:search]   
-        @overviews = Post.includes(:user).search_overview(params[:search], params[:page], params[:sort]).where(" drop_off_avaibility_start_date >= ?",Date.today)
+        @overviews = Post.includes(:user).search_overview(params[:search], params[:page], params[:sort]).where(" drop_off_avaibility_start_date >= ? and status = ?",Date.today,true)
         @posts = Post.includes(:user).search(params[:search], params[:page], params[:sort]).where(" drop_off_avaibility_start_date >= ?",Date.today)
       else
         @overviews = Post.includes(:user).order(sort_column + " " + sort_direction).where(" drop_off_avaibility_start_date >= ?",Date.today)
-        @posts = Post.includes(:user).page(params[:page]).per_page(4).order(sort_column + " " + sort_direction).where(" drop_off_avaibility_start_date >= ?",Date.today)
+        @posts = Post.includes(:user).page(params[:page]).per_page(4).order(sort_column + " " + sort_direction).where(" drop_off_avaibility_start_date >= ? and status = ?",Date.today,true)
       end
     respond_to do |format|
       format.html # index.html.erb
