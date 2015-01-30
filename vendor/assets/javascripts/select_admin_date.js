74ff9fa318fef6a516c920adb03eb147c5225b7b
Amol <rails@kunalinfotech.net>
2015-01-30 17:20:50 +0530

    selected datepicker bug- admin side

---
+++ vendor/assets/javascripts/select_admin_date.js
@@ -0,0 +1,7 @@
+ 	jQuery(function($){
+      $("#start_date").datepicker({dateFormat: 'yy-mm-dd',changeMonth: false,changeYear: false,showOn: 'button', buttonImage: 'http://jqueryui.com/resources/demos/datepicker/images/calendar.gif', buttonImageOnly: true})
+
+    });
+    jQuery(function($){
+      $("#end_date").datepicker({dateFormat: 'yy-mm-dd',changeMonth: false,changeYear: false, showOn: 'button', buttonImage: 'http://jqueryui.com/resources/demos/datepicker/images/calendar.gif', buttonImageOnly: true})
+    });
