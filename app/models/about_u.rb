318ff3bbe2ec56a899b074889e19ff5785cb659c
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-02-06 14:19:38 +0300

    Configure paperclip correctly.

--- app/models/about_u.rb
+++ app/models/about_u.rb
@@ -1,12 +1,11 @@
 class AboutU < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { :thumb => "61x61" },
-  			:storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml",
-            :path => "/estore_management/about_us/:id/:style/:basename.:extension",
-            :convert_options => {
-                          :thumb => "-compose Copy -gravity center -extent 61x61"
-                      }
+    :path => "/estore_management/about_us/:id/:style/:basename.:extension",
+    :convert_options => {
+      :thumb => "-compose Copy -gravity center -extent 61x61"
+    }
 
-  	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+    validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
 end
