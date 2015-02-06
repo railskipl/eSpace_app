03343ca0d85e371de9492d0041a163815abf36b7
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-02-06 17:30:30 +0300

    Remove unused eager loading.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,11 +64,11 @@ class PostsController < ApplicationController
 
   def overview
     if params[:search]
-      @overviews = Post.includes(:user).search_overview(params[:search], params[:page], params[:sort])
-      @posts = Post.includes(:user).search(params[:search], params[:page], params[:sort])
+      @overviews = Post.search_overview(params[:search], params[:page], params[:sort])
+      @posts = Post.search(params[:search], params[:page], params[:sort])
     else
-      @overviews = Post.includes(:user).order(sort_order).where(" drop_off_avaibility_end_date >= ? and status = ? and archive = ? and area_available >= ?",Date.today, true,false,4)
-      @posts = Post.includes(:user).page(params[:page]).per_page(4).order(sort_order).where(" drop_off_avaibility_end_date >= ? and status = ? and archive = ? and area_available >= ?",Date.today, true,false,4)
+      @overviews = Post.order(sort_order).where(" drop_off_avaibility_end_date >= ? and status = ? and archive = ? and area_available >= ?",Date.today, true,false,4)
+      @posts = Post.page(params[:page]).per_page(4).order(sort_order).where(" drop_off_avaibility_end_date >= ? and status = ? and archive = ? and area_available >= ?",Date.today, true,false,4)
     end
     respond_to do |format|
       format.html # index.html.erb
