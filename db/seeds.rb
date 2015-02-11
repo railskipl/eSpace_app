e8c7d298b945865b70948a384dc8aa2d43759818
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-02-11 17:04:50 +0300

    Remove admin in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-User.reset_column_information
-admin = User.new(:email => 'support@dinobo.com', :password => 'sxP7WsW3CdjRd3PV4CnX', :password_confirmation => 'sxP7WsW3CdjRd3PV4CnX', :admin => 'true')
-admin.skip_confirmation!
-admin.save!
-
 puts 'SETTING UP DEFAULT EMAIL DOMAIN ALLOWED FOR SIGNUP'
 AccessId.reset_column_information
 AccessId.create! email: 'usc.edu'
